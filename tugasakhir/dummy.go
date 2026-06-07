ee4544b196821b13bf6b67c525410e8e7e5cbf9b
Deweh <diksatyawilwadarma@gmail.com>
2026-06-07 22:40:15 +0700

    Perubahan Dummy

--- tugasakhir/dummy.go
+++ tugasakhir/dummy.go
@@ -6,8 +6,8 @@ func inisialisasiDataDummy() {
 	dataKandidat[2] = Kandidat{nomorUrut: 3, nama: "Ilman Baruna", visiMisi: "Kudus Negeri Kretek", jumlahSuara: 0}
 	jumlahKandidat = 3
 
-	dataPemilih[0] = Pemilih{idPemilih: 101, nama: "Siti Aminah", sudahMemilih: true}
-	dataPemilih[1] = Pemilih{idPemilih: 102, nama: "Raka Saputra", sudahMemilih: true}
+	dataPemilih[0] = Pemilih{idPemilih: 101, nama: "Fathir Muhammad", sudahMemilih: true}
+	dataPemilih[1] = Pemilih{idPemilih: 102, nama: "Fiandra Gaming", sudahMemilih: true}
 	dataPemilih[2] = Pemilih{idPemilih: 103, nama: "Dini Lestari", sudahMemilih: true}
 	dataPemilih[3] = Pemilih{idPemilih: 104, nama: "Bima Arya", sudahMemilih: false}
 	dataPemilih[4] = Pemilih{idPemilih: 105, nama: "Nadia Putri", sudahMemilih: false}
